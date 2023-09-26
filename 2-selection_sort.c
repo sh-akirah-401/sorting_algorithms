ebb2d5a8c8c4790e96082f7a3e9c1b35b4d53fdf
Ojay16 <juliusjeff16@gmail.com>
2023-09-25 17:48:16 -0700

    updated

---
+++ 2-selection_sort.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+
+/**
+ * swap_ints - Swaps two ints in an array.
+ * @a: First int to swap.
+ * @b: Second int to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * selection_sort - Sorts a array of ints in ascending order
+ *                  using d selection sort algorithm.
+ * @array: array of ints.
+ * @size: Size of the array.
+ *
+ * Description: Prints an  array after each swap.
+ */
+void selection_sort(int *array, size_t size)
+{
+	int *min;
+	size_t i, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = array + i;
+		for (j = i + 1; j < size; j++)
+			min = (array[j] < *min) ? (array + j) : min;
+
+		if ((array + i) != min)
+		{
+			swap_ints(array + i, min);
+			print_array(array, size);
+		}
+	}
+}
